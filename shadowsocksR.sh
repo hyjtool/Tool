d9a3559e1403a89004b7740900a0f6cd835d7132
hyjtool <davidho.contact@gmail.com>
2017-04-10 20:19:55 +0800

    Update shadowsocksR.sh

--- shadowsocksR.sh
+++ shadowsocksR.sh
@@ -32,48 +32,37 @@ bash initcfg.sh
 config_shadowsocks(){
     cat > /etc/shadowsocksr/user-config.json<<-EOF
 {
-    "server":"0.0.0.0",
-    "server_ipv6":"::",
-    "server_port":${shadowsocksport},
-    "local_address":"127.0.0.1",
-    "local_port":1080,
-    "password":"${shadowsockspwd}",
-    "timeout":120,
-    "method":"rc4-md5",
-    "protocol":"auth_sha1_v4",
-    "protocol_param":"",
-    "obfs":"plain",
-    "obfs_param":"",
-    "redirect":"",
-    "dns_ipv6":false,
-    "fast_open":false,
-    "workers":1
+    "server": "0.0.0.0",
+    "server_ipv6": "::",
+    "server_port": 443,
+    "local_address": "127.0.0.1",
+    "local_port": 1080,
+
+    "password": "ilovessr",
+    "method": "rc4-md5",
+    "protocol": "auth_aes128_md5",
+    "protocol_param": "",
+    "obfs": "plain",
+    "obfs_param": "",
+    "speed_limit_per_con": 0,
+    "speed_limit_per_user": 0,
+
+    "additional_ports" : {}, // only works under multi-user mode
+    "timeout": 120,
+    "udp_timeout": 60,
+    "dns_ipv6": false,
+    "connect_verbose_info": 0,
+    "redirect": "",
+    "fast_open": false
 }
+
 EOF
 }
 
+cd ~/shadowsocksr/shadowsocks
+python server.py -d start
 
-
-        /etc/init.d/shadowsocks start
-
-        clear
-        echo
-        echo "Congratulations, ShadowsocksR install completed!"
-        echo -e "Server IP: \033[41;37m $(get_ip) \033[0m"
-        echo -e "Server Port: \033[41;37m ${shadowsocksport} \033[0m"
-        echo -e "Password: \033[41;37m ${shadowsockspwd} \033[0m"
-        echo -e "Local IP: \033[41;37m 127.0.0.1 \033[0m"
-        echo -e "Local Port: \033[41;37m 1080 \033[0m"
-        echo -e "Protocol: \033[41;37m auth_sha1_v4 \033[0m"
-        echo -e "obfs: \033[41;37m plain \033[0m"
-        echo -e "Encryption Method: \033[41;37m rc4-md5 \033[0m"
-        echo
-        echo "Welcome to visit:https://shadowsocks.be/9.html"
-        echo "If you want to change protocol & obfs, please visit reference URL:"
-        echo "https://github.com/breakwa11/shadowsocks-rss/wiki/Server-Setup"
-        echo
-        echo "Enjoy it!"
-        echo
+       
     
-}
+
 
