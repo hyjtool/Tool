984625454f695ea73da4014c600dbeb0396b913c
hyjtool <davidho.contact@gmail.com>
2017-04-10 22:24:24 +0800

    Update lkl.sh

--- lkl.sh
+++ lkl.sh
@@ -21,4 +21,13 @@ cd /root/shadowsocksr/shadowsocks
 LD_PRELOAD=~/lkl/liblkl-hijack.so LKL_HIJACK_NET_QDISC="root|fq" LKL_HIJACK_SYSCTL='net.ipv4.tcp_congestion_control="bbr";net.ipv4.tcp_wmem="4096 16384 30000000"' LKL_HIJACK_NET_IFTYPE=tap LKL_HIJACK_NET_IFPARAMS=tap0 LKL_HIJACK_NET_IP=10.0.0.2 LKL_HIJACK_NET_NETMASK_LEN=24 LKL_HIJACK_NET_GATEWAY=10.0.0.1 LKL_HIJACK_OFFLOAD="0x8883" python server.py
 
 
+#写入自动启动
+cat > /root/lkl/run.sh<<-EOF
+cd /root/shadowsocksr/shadowsocks
+LD_PRELOAD=~/lkl/liblkl-hijack.so LKL_HIJACK_NET_QDISC="root|fq" LKL_HIJACK_SYSCTL='net.ipv4.tcp_congestion_control="bbr";net.ipv4.tcp_wmem="4096 16384 30000000"' LKL_HIJACK_NET_IFTYPE=tap LKL_HIJACK_NET_IFPARAMS=tap0 LKL_HIJACK_NET_IP=10.0.0.2 LKL_HIJACK_NET_NETMASK_LEN=24 LKL_HIJACK_NET_GATEWAY=10.0.0.1 LKL_HIJACK_OFFLOAD="0x8883" python server.py
+EOF
+
+chmod +x run.sh
+sed -i "s/exit 0/ /ig" /etc/rc.local
+echo "/root/lkl/run.sh" >> /etc/rc.local
 
