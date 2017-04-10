df3fcea0c71606ab7decda9c3eca0485b211b75f
hyjtool <davidho.contact@gmail.com>
2017-04-10 21:55:18 +0800

    Update lkl.sh

--- lkl.sh
+++ lkl.sh
@@ -7,7 +7,8 @@ export PATH
 #   Thanks: @allient neko                                         #
 #=================================================================#
 
-
+#Current folder
+cur_dir=`pwd`
 
 mkdir /root/lkl
 cd /root/lkl
@@ -15,29 +16,12 @@ cd /root/lkl
 	
 wget --no-check-certificate https://raw.githubusercontent.com/hyjtool/Tool/master/liblkl-hijack2.so
 
+
 cat > /root/lkl/lkl.sh<<-EOF
-LD_PRELOAD=/root/lkl/liblkl-hijack.so LKL_HIJACK_NET_QDISC="root|fq" LKL_HIJACK_SYSCTL="net.ipv4.tcp_congestion_control=bbr" LKL_HIJACK_NET_IFTYPE=tap LKL_HIJACK_NET_IFPARAMS=lkl-tap LKL_HIJACK_NET_IP=10.0.0.2 LKL_HIJACK_NET_NETMASK_LEN=24 LKL_HIJACK_NET_GATEWAY=10.0.0.1 haproxy -f /root/lkl/haproxy.cfg
+LD_PRELOAD=~/lkl/liblkl-hijack2.so LKL_HIJACK_NET_QDISC="root|fq" LKL_HIJACK_SYSCTL='net.ipv4.tcp_congestion_control="bbr";net.ipv4.tcp_wmem="4096 16384 30000000"' LKL_HIJACK_NET_IFTYPE=tap LKL_HIJACK_NET_IFPARAMS=tap0 LKL_HIJACK_NET_IP=10.0.0.2 LKL_HIJACK_NET_NETMASK_LEN=24 LKL_HIJACK_NET_GATEWAY=10.0.0.1 LKL_HIJACK_OFFLOAD="0x8883" python server.py
 EOF
 
 chmod +x lkl.sh
 
-
-#写入自动启动
-if [[ "$release" = "CentOS" && "$ver" = "7" ]]; then
-	echo "/root/lkl/run.sh" >> /etc/rc.d/rc.local
-	chmod +x /etc/rc.d/rc.local
-else
-	sed -i "s/exit 0/ /ig" /etc/rc.local
-	echo "/root/lkl/run.sh" >> /etc/rc.local
-fi
-
-
-./run.sh
-
-#判断是否启动
-p=`ping 10.0.0.2 -c 3 | grep ttl`
-if [ "$p" == "" ]; then
-	echo "fail"
-else
-	echo "success"
-fi
+cd /root/shadowsocksr/shadowsocks
+./lkl.sh
