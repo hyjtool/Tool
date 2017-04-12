9d16efe96ec66f1a5bfb4c74f3557f41ee29897f
hyjtool <davidho.contact@gmail.com>
2017-04-12 12:25:04 +0800

    Update lkl.sh

--- lkl.sh
+++ lkl.sh
@@ -7,51 +7,66 @@ export PATH
 #   Thanks :         @linhua , @allientNeko                       #
 #=================================================================#
 
-#创建文件夹
+apt-get update
+apt-get install -y haproxy
+
 mkdir /root/lkl
 cd /root/lkl
+cat > /root/lkl/haproxy.cfg<<-EOF
+global
+defaults
+log global
+mode tcp
+option dontlognull
+timeout connect 5000
+timeout client 50000
+timeout server 50000
+frontend proxy-in
+bind *:443
+default_backend proxy-out
+backend proxy-out
+server server1 10.0.0.1 maxconn 20480
+EOF
+	
+wget --no-check-certificate http://soft.91yun.org/uml/lkl/liblkl-hijack.so
 
-#下载脚本
-wget --no-check-certificate https://raw.githubusercontent.com/hyjtool/Tool/master/liblkl-hijack.so
-
-#配置文件
-cat > /root/lkl/bbr.sh<<-EOF
-#!/bin/bash
-
-fuser -k /dev/net/tun
-# delete tap0
-ip tuntap del dev tap0 mode tap
-
-# add tap0
-ip tuntap add dev tap0 mode tap
-ip link set dev tap0 up
-ip addr add dev tap0 10.0.0.1/24
-sysctl -w net.ipv4.ip_forward=1
-iptables -P FORWARD ACCEPT
-
-# delete old iptables rules
-iptables -t nat -D POSTROUTING -o venet0 -j MASQUERADE
-iptables -t nat -D PREROUTING -i venet0 -p tcp --dport 443 -j DNAT --to-destination 10.0.0.2
-
-
-# add iptables rules
-iptables -t nat -A POSTROUTING -o venet0 -j MASQUERADE
-iptables -t nat -A PREROUTING -i venet0 -p tcp --dport 443 -j DNAT --to-destination 10.0.0.2
-
-
-LD_PRELOAD="/root/lkl/liblkl-hijack.so" LKL_HIJACK_NET_QDISC="root|fq" LKL_HIJACK_SYSCTL='net.ipv4.tcp_congestion_control="bbr";net.ipv4.tcp_wmem="4096 16384 30000000"' LKL_HIJACK_NET_IFTYPE="tap" LKL_HIJACK_NET_IFPARAMS="tap0" LKL_HIJACK_NET_IP="10.0.0.2" LKL_HIJACK_NET_NETMASK_LEN="24" LKL_HIJACK_NET_GATEWAY="10.0.0.1" LKL_HIJACK_OFFLOAD="0x8883" $* &
-
-exit
-
+cat > /root/lkl/lkl.sh<<-EOF
+LD_PRELOAD=/root/lkl/liblkl-hijack.so LKL_HIJACK_NET_QDISC="root|fq" LKL_HIJACK_SYSCTL="net.ipv4.tcp_congestion_control=bbr" LKL_HIJACK_NET_IFTYPE=tap LKL_HIJACK_NET_IFPARAMS=lkl-tap LKL_HIJACK_NET_IP=10.0.0.2 LKL_HIJACK_NET_NETMASK_LEN=24 LKL_HIJACK_NET_GATEWAY=10.0.0.1 haproxy -f /root/lkl/haproxy.cfg
 EOF
 
-#给予权限
-chmod +x bbr.sh
 
-#启动
-cd /root/shadowsocksr/shadowsocks
-/root/lkl/bbr.sh python3 server.py
 
-#清理
-rm -rf /root/lkl.sh
+cat > /root/lkl/run.sh<<-EOF
+ip tuntap add lkl-tap mode tap
+ip addr add 10.0.0.1/24 dev lkl-tap
+ip link set lkl-tap up
+sysctl -w net.ipv4.ip_forward=1
+iptables -P FORWARD ACCEPT 
+iptables -t nat -A POSTROUTING -o venet0 -j MASQUERADE
+iptables -t nat -A PREROUTING -i venet0 -p tcp --dport 443 -j DNAT --to-destination 10.0.0.2
+nohup /root/lkl/lkl.sh &
+p=\`ping 10.0.0.2 -c 3 | grep ttl\`
+if [ \$? -ne 0 ]; then
+	echo "success "\$(date '+%Y-%m-%d %H:%M:%S') > /root/lkl/log.log
+else
+	echo "fail "\$(date '+%Y-%m-%d %H:%M:%S') > /root/lkl/log.log
+fi
+EOF
 
+
+chmod +x lkl.sh
+chmod +x run.sh
+
+#写入自动启动
+sed -i "s/exit 0/ /ig" /etc/rc.local
+echo "/root/lkl/run.sh" >> /etc/rc.local
+
+./run.sh
+
+#判断是否启动
+p=`ping 10.0.0.2 -c 3 | grep ttl`
+if [ "$p" == "" ]; then
+	echo "fail"
+else
+	echo "success"
+fi
