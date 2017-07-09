9af440f3d13126d0756983eeb3be8265420f8faa
hyjtool <davidho.contact@gmail.com>
2017-07-09 11:59:56 +0800

    Update lkl.sh

--- lkl.sh
+++ lkl.sh
@@ -3,7 +3,7 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 #=================================================================#
 #   Description :      Linux kernel library                       #
-#   System Required :     Debian 8.0 x86_64                       #
+#   System Required :     Centos 7.0 x86_64                       #
 #   Thanks :         @linhua , @allientNeko                       #
 #=================================================================#
 
