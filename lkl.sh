76480b7e4158a14d0069e2af1df4fafaff2dd363
hyjtool <davidho.contact@gmail.com>
2017-07-15 13:36:07 +0800

    Update lkl.sh

--- lkl.sh
+++ lkl.sh
@@ -7,66 +7,67 @@ export PATH
 #   Thanks :         @linhua , @allientNeko                       #
 #=================================================================#
 
-#安装haproxy
-yum install -y haproxy
-
 #创建文件夹
 mkdir /root/lkl
 cd /root/lkl
 
+
 #下载liblkl-hijack.so
 wget --no-check-certificate https://raw.githubusercontent.com/hyjtool/Tool/master/liblkl-hijack.so
 
-#设定tuntap和NAT
-cat > /root/lkl/run.sh<<-EOF
+
+#写入bbr.sh
+cat > /root/lkl/bbr.sh<<-EOF
+#!/bin/bash
+
+fuser -k /dev/net/tun
+
+# delete tap0
+ip tuntap del dev tap0 mode tap
+
+# add tap0
+ip tuntap add dev tap0 mode tap
+ip link set dev tap0 up
+ip addr add dev tap0 10.0.0.1/24
 sysctl -w net.ipv4.ip_forward=1
-ip tuntap add lkl-tap mode tap
-ip addr add 10.0.0.1/24 dev lkl-tap
-ip link set lkl-tap up
-iptables -P FORWARD ACCEPT 
+iptables -P FORWARD ACCEPT
+
+# delete old iptables rules
+iptables -t nat -D POSTROUTING -o venet0 -j MASQUERADE
+iptables -t nat -D PREROUTING -i venet0 -p tcp --dport 443 -j DNAT --to-destination 10.0.0.2
+
+# add iptables rules
 iptables -t nat -A POSTROUTING -o venet0 -j MASQUERADE
 iptables -t nat -A PREROUTING -i venet0 -p tcp --dport 443 -j DNAT --to-destination 10.0.0.2
 
-nohup /root/lkl/lkl.sh &
+LD_PRELOAD="/root/lkl/liblkl-hijack.so" LKL_HIJACK_NET_QDISC="root|fq" LKL_HIJACK_SYSCTL="net.ipv4.tcp_congestion_control=bbr;net.ipv4.tcp_wmem=4096 16384 30000000" LKL_HIJACK_NET_IFTYPE="tap" LKL_HIJACK_NET_IFPARAMS="tap0" LKL_HIJACK_NET_IP="10.0.0.2" LKL_HIJACK_NET_NETMASK_LEN="24" LKL_HIJACK_NET_GATEWAY="10.0.0.1" LKL_HIJACK_OFFLOAD="0x8883" $* &
+
+exit
 
 EOF
 
-#写入haproxy.cfg
-cat > /root/lkl/haproxy.cfg<<-EOF
-global
-pidfile /var/run/haproxy.pid
-ulimit-n 15000
 
-defaults
-log global
-mode tcp
-option dontlognull
-timeout connect 1000
-timeout client 150000
-timeout server 150000
+#写入run.sh
+cat > /root/lkl/run.sh<<-EOF
+#!/bin/bash
+~/bbr.sh /usr/bin/python /root/shadowsocksr/shadowsocks/server.py --pid-file /var/run/shadowsocks.pid -c /root/shadowsocksr/user-config.json
 
-frontend proxy-in
-bind *:443
-default_backend proxy-out
-
-backend proxy-out
-server server1 10.0.0.1 maxconn 20480
 EOF
 
-#写入lkl.sh
-cat > /root/lkl/lkl.sh<<-EOF
-LD_PRELOAD=/root/lkl/liblkl-hijack.so LKL_HIJACK_NET_QDISC="root|fq" LKL_HIJACK_SYSCTL="net.ipv4.tcp_congestion_control=bbr;net.ipv4.tcp_wmem=4096 16384 30000000" LKL_HIJACK_OFFLOAD="0x9983" LKL_HIJACK_NET_IFTYPE=tap LKL_HIJACK_NET_IFPARAMS=lkl-tap LKL_HIJACK_NET_IP=10.0.0.2 LKL_HIJACK_NET_NETMASK_LEN=24 LKL_HIJACK_NET_GATEWAY=10.0.0.1 haproxy -f /root/lkl/haproxy.cfg
-EOF
 
 #给予权限
-chmod +x lkl.sh
+chmod +x bbr.sh
 chmod +x run.sh
+
 
 #开机自启
 echo "/root/lkl/run.sh" >> /etc/rc.d/rc.local
 chmod +x /etc/rc.d/rc.local
 
+
+#启动
 ./run.sh
+
 
 #检查启动
 p=`ping 10.0.0.2 -c 5 | grep ttl`
