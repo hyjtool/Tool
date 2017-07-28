49003f791f270cb07e953e180ebf60cdc6c0f00c
hyjtool <davidho.contact@gmail.com>
2017-07-28 17:34:08 +0800

    Update shadowsocksR.sh

--- shadowsocksR.sh
+++ shadowsocksR.sh
@@ -36,7 +36,7 @@ get_char(){
 
 yum install -y git
 
-git clone -b manyuser https://github.com/shadowsocksr/shadowsocksr.git
+git clone -b manyuser https://github.com/hyjtool/shadowsocksr.git
 
 cd ~/shadowsocksr
 
