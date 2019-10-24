7ba6267d0b6e1a8f4cf80685778c173030951447
hyjtool <greggho.contact@gmail.com>
2019-10-24 08:39:37 +0800

    Update V2RayCaddy.sh

--- V2RayCaddy.sh
+++ V2RayCaddy.sh
@@ -15,21 +15,9 @@ echo "#         System Required: Centos 8 x86_64                   #"
 echo "#                 Thanks:  Myself                            #"
 echo "#############################################################"
 echo
-
-# Info
-get_char(){
-        SAVEDSTTY=`stty -g`
-        stty -echo
-        stty cbreak
-        dd if=/dev/tty bs=1 count=1 2> /dev/null
-        stty -raw
-        stty echo
-        stty $SAVEDSTTY
-    }
-    echo
-    echo "Press Enter to continue...or Press Ctrl+C to cancel"
-    char=`get_char`
-    
+echo
+echo
+echo  
 read -p "请输入你的域名:" domain
 
 # 安装V2Ray
