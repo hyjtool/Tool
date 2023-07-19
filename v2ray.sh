08dbc288a2e60e20afd080b56adc88851088ac83
hyjtool <greggho.contact@gmail.com>
2023-07-19 13:36:22 +0800

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -71,7 +71,7 @@ cat > /usr/local/etc/v2ray/config.json<<-EOF
                 ]
             },
             "streamSettings": {
-                "network": "ws"
+		"network": "ws"
             }
         },
         {
@@ -86,7 +86,7 @@ cat > /usr/local/etc/v2ray/config.json<<-EOF
                 ]
             },
             "streamSettings": {
-                "network": "ws",
+		"network": "ws",
 		"security": "none"
             }
         }
