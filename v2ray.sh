af4f0ecb749cbbd68a557ba26f6cf7e570a5a7a6
hyjtool <greggho.contact@gmail.com>
2023-07-30 16:48:37 +0800

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -14,20 +14,6 @@ echo
 echo
 echo  
 
-# Info
-get_char(){
-        SAVEDSTTY=`stty -g`
-        stty -echo
-        stty cbreak
-        dd if=/dev/tty bs=1 count=1 2> /dev/null
-        stty -raw
-        stty echo
-        stty $SAVEDSTTY
-    }
-    echo
-    echo "Press Enter to continue...or Press Ctrl+C to cancel"
-    char=`get_char`
-
 read -p "请输入你的用户ID:" ID
 
 # 安装v2ray
