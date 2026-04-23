f6ce30022d5cf05da1adcfb6106bbcc9830fe29e
hyjtool <greggho.contact@gmail.com>
2026-04-23 09:23:59 +0800

    Add files via upload

---
+++ Hysteria2.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+clear
+echo
+echo "#############################################################"
+echo "#                       v2ray                                #"
+echo "#         System Required: Debian 10 x86_64                  #"
+echo "#        Some birds are not meant to be caged                #"
+echo "#############################################################"
+echo
+echo
+echo
+echo  
+read -p "1.请输入你的端口:" port
+echo 
+echo 
+read -p "2.请输入你的密码:" password
+echo 
+echo 
+
+# 安装v2ray
+apt update
+
+apt install curl
+
+apt install vim
+
+touch ~/.vimrc
+
+bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
+
+rm -rf /usr/local/etc/v2ray/config.json
+
+cat > /usr/local/etc/v2ray/config.json<<-EOF
+{
+    "log": {
+        "loglevel": "warning"
+    },
+    "inbounds": [
+        {
+            "port": $port,
+            "protocol": "trojan",
+            "settings": {
+                "clients": [
+                    {
+                        "password":"$password",
+                        "email": "love@v2fly.org"
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": "tcp",
+                "security": "tls",
+                "tlsSettings": {
+                    "alpn": [
+                        "http/1.1"
+                    ],
+                    "certificates": [
+                        {
+                            "certificateFile": "/etc/ssl/cert.crt",
+                            "keyFile": "/etc/ssl/key.key"
+                        }
+                    ]
+                }
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom",
+            "tag": "direct"
+        }
+    ]
+}
+EOF
+
+# 开机自启
+systemctl enable v2ray
+systemctl start v2ray
